00623641c6593410f31e9705931bd1cc8e56f303
1E0N4RD <welsch.leonard@protonmail.com>
2025-11-23 17:46:37 +0100

    Finished flake.nix

--- Makefile
+++ Makefile
@@ -1,10 +1,14 @@
 CFLAGS := -std=c99 -Wall -Wextra -Werror -Wpedantic -Og -g
-CC := cc $(CFLAGS)
+CFLAGS += $(shell pkg-config --cflags --libs dbus-1)
+CC += $(CFLAGS)
 
 TARGETS := auto-power-profile
 COMMON_HEADERS := log.h
 
 all: $(TARGETS)
+
+.PHONY:
+	install
 
 %.o: %.c %.h
 	$(CC) -c -o $@ $<
@@ -17,6 +21,10 @@ auto-power-profile: auto-power-profile.o log.o
 
 run: auto-power-profile
 	./auto-power-profile
+
+install: auto-power-profile
+	mkdir -p $(PREFIX)/bin
+	cp -t $(PREFIX)/bin auto-power-profile
 
 clean:
 	-rm *.o
--- flake.nix
+++ flake.nix
@@ -5,26 +5,30 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs }: 
   let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
   in {
     devShells.${system}.default = pkgs.mkShell {
-      buildInputs = with pkgs; [
+      packages = with pkgs; [
         gcc
         gnumake
         bear
         clang-tools
         fish
-        dbus
-        dbus.lib
+        dbus.dev
+        pkg-config
       ];
 
-      shellHook = ''
-        exec fish
-      '';
-      NIX_CFLAGS_COMPILE=''-isystem ${pkgs.dbus.dev}/include/dbus-1.0 -isystem ${pkgs.dbus.lib}/lib/dbus-1.0/include'';
+      shellHook = "exec fish";
+    };
+    packages.${system}.default = pkgs.stdenv.mkDerivation {
+      name = "auto-power-mode";
+      nativeBuildInputs = [ pkgs.pkg-config ];
+      buildInputs = [ pkgs.dbus.dev ];
+      makeFlags = [ "PREFIX=${placeholder "out"}"];
+      src = ./.;
     };
   };
 }
